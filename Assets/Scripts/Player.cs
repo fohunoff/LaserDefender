91a2d3ff080b8b875867641600a6a4fbabb55105
Ivan Zagaynov <fohunoff@gmail.com>
2022-06-08 16:30:32 +0300

    init and add Input System

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    Vector2 rawInput;
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+}
